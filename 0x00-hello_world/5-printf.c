e30c0abb788daa5d9894b69de53fa514c8acb03e
mary-omari <maryomari93@gmail.com>
2022-09-08 15:06:50 -0700

    program that prints using function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * prints the statement
+ *
+ * Return: Always 0 (Succes)
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
