dbc22bb0245a529d814a732b61011de3aedc712b
mary-omari <maryomari93@gmail.com>
2022-12-13 11:34:16 -0800

    task 0

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of an integer.
+ * @n: integer to be computed.
+ *
+ * Return: Absolute value of an integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
